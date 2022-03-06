fff8381b1e7a64851a3bc011e54cba16e6fb15c8
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 22:44:34 +0300

    Switch to sql

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from distutils.command.config import config
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 from app import routes, models, errors
