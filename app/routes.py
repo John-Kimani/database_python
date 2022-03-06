0edc70dd0ce35ffe6b8163643010a9251f7dd3d2
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 21:43:19 +0300

    Redirects to login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm, RegistrationForm
 
 
@@ -10,14 +11,24 @@ def index():
 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('index')
     return render_template('login.html', title='Sign In', form=form)
 
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Sign up requested for user {}'.format(
+            form.username.data
+        ))
+        return redirect('login')
     return render_template('signup.html', title="Signup", form=form)
