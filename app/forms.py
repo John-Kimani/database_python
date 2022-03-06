a13f0081ecccd74cc8b1a53df083a14c67f6f561
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 21:34:42 +0300

    Sign up form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
 
@@ -9,3 +9,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(1, 10)])
+    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
@@ -14,3 +14,10 @@ def index():
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
+
+
+
+@app.route('/signup')
+def signup():
+    form = RegistrationForm()
+    return render_template('signup.html', title="Signup", form=form)
