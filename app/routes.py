e75d4b60a41b5d37cef1bfa90fd0435d562938de
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 23:43:01 +0300

    Login user

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from crypt import methods
-from app import app
-from flask import render_template, flash, redirect
+from app import app,db
+from flask import render_template, flash, redirect,url_for
 from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -13,22 +15,39 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect('index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+        # flash('Login requested for user {}, remember_me={}'.format(
+        #     form.username.data, form.remember_me.data
+        # ))
+        # return redirect('index')
     return render_template('login.html', title='Sign In', form=form)
 
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Sign up requested for user {}'.format(
-            form.username.data
-        ))
+        user = User(username=form.username.data, email=form.email.data, password_hash=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulation you are now a registered user!')
         return redirect('login')
     return render_template('signup.html', title="Signup", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
